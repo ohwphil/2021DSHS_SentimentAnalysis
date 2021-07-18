2dd26d3a579514463643eee912cb95f1866f5304
ohwphil <71064896+ohwphil@users.noreply.github.com>
2021-07-19 00:54:04 +0900

    Create run_bert.py

---
+++ run_bert.py
@@ -0,0 +1,202 @@
+import json
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import gluonnlp as nlp
+import numpy as np
+from tqdm import tqdm, tqdm_notebook
+
+from kobert.utils import get_tokenizer
+from kobert.pytorch_kobert import get_pytorch_kobert_model
+
+from transformers import AdamW
+from transformers.optimization import get_cosine_schedule_with_warmup
+
+
+with open("dataset_original_json/texts.json", "r", encoding="UTF-8") as fp:
+    document_gpt2 = json.load(fp)
+with open("dataset_augmented_json/texts_augmented.json", "r", encoding="UTF-8") as fp:
+    document_gpt2 += json.load(fp)
+
+with open("dataset_original_json/labels.json", "r", encoding="UTF-8") as fp:
+    labels = json.load(fp)
+with open("dataset_augmented_json/labels_augmented.json", "r", encoding="UTf-8") as fp:
+    labels += json.load(fp)
+
+for i in range(len(document_gpt2)):
+  document_gpt2[i]=[document_gpt2[i], labels[i]]
+
+document_gpt2[:5]
+
+from sklearn.model_selection import train_test_split
+
+dataset_train, dataset_test, _, _ = train_test_split(document_gpt2, labels, random_state=42, test_size=0.3)
+##GPU 사용 시
+device = torch.device("cuda:0")
+
+import os
+from zipfile import ZipFile
+
+import torch
+
+from transformers import BertModel
+import gluonnlp as nlp
+
+from kobert.utils import download as _download
+from kobert.utils import tokenizer
+
+pytorch_kobert = {
+    'url':
+    'https://kobert.blob.core.windows.net/models/kobert/pytorch/kobert_v1.zip',
+    'fname': 'kobert_v1.zip',
+    'chksum': '411b242919'  # 411b2429199bc04558576acdcac6d498
+}
+
+
+def get_pytorch_kobert_model(ctx='cpu', cachedir='~/kobert/'):
+    # download model
+    model_info = pytorch_kobert
+    model_down = _download(model_info['url'],
+                           model_info['fname'],
+                           model_info['chksum'],
+                           cachedir=cachedir)
+    
+    zipf = ZipFile(os.path.expanduser(model_down))
+    zipf.extractall(path=os.path.expanduser(cachedir))
+    model_path = os.path.join(os.path.expanduser(cachedir), 'kobert_from_pretrained')
+    # download vocab
+    vocab_info = tokenizer
+    vocab_path = _download(vocab_info['url'],
+                           vocab_info['fname'],
+                           vocab_info['chksum'],
+                           cachedir=cachedir)
+    return get_kobert_model(model_path, vocab_path, ctx)
+
+
+def get_kobert_model(model_path, vocab_file, ctx="cpu"):
+    bertmodel = BertModel.from_pretrained(model_path)
+    device = torch.device(ctx)
+    bertmodel.to(device)
+    bertmodel.eval()
+    vocab_b_obj = nlp.vocab.BERTVocab.from_sentencepiece(vocab_file,
+                                                         padding_token='[PAD]')
+    return bertmodel, vocab_b_obj
+
+bertmodel, vocab = get_pytorch_kobert_model()
+
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
+                 pad, pair):
+        transform = nlp.data.BERTSentenceTransform(
+            bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return (self.sentences[i] + (self.labels[i], ))
+
+    def __len__(self):
+        return (len(self.labels))
+
+## Setting parameters
+max_len = 64
+batch_size = 64
+warmup_ratio = 0.1
+num_epochs = 5
+max_grad_norm = 1
+log_interval = 200
+learning_rate =  5e-5
+
+data_train = BERTDataset(dataset_train, 0, 1, tok, max_len, True, False)
+data_test = BERTDataset(dataset_test, 0, 1, tok, max_len, True, False)
+
+train_dataloader = torch.utils.data.DataLoader(data_train, batch_size=batch_size, num_workers=5)
+test_dataloader = torch.utils.data.DataLoader(data_test, batch_size=batch_size, num_workers=5)
+
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                 bert,
+                 hidden_size = 768,
+                 num_classes=7,
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+                 
+        self.classifier = nn.Linear(hidden_size , num_classes)#앞에 linear layer를 붙여서 만듦
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+    
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
+
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+        
+        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+
+model = BERTClassifier(bertmodel, dr_rate=0.5).to(device)
+
+# Prepare optimizer and schedule (linear warmup and decay)
+no_decay = ['bias', 'LayerNorm.weight']
+optimizer_grouped_parameters = [
+    {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+    {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+]
+
+optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate)
+loss_fn = nn.CrossEntropyLoss()
+
+t_total = len(train_dataloader) * num_epochs
+warmup_step = int(t_total * warmup_ratio)
+
+scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=warmup_step, num_training_steps=t_total)
+
+def calc_accuracy(X,Y):
+    max_vals, max_indices = torch.max(X, 1)
+    train_acc = (max_indices == Y).sum().data.cpu().numpy()/max_indices.size()[0]
+    return train_acc
+
+for e in range(num_epochs):
+    train_acc = 0.0
+    test_acc = 0.0
+    model.train()
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm_notebook(train_dataloader)):
+        optimizer.zero_grad()
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+        valid_length= valid_length
+        label = label.long().to(device)
+        out = model(token_ids, valid_length, segment_ids)
+        loss = loss_fn(out, label)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+        optimizer.step()
+        scheduler.step()  # Update learning rate schedule
+        train_acc += calc_accuracy(out, label)
+        if batch_id % log_interval == 0:
+            print("epoch {} batch id {} loss {} train acc {}".format(e+1, batch_id+1, loss.data.cpu().numpy(), train_acc / (batch_id+1)))
+    print("epoch {} train acc {}".format(e+1, train_acc / (batch_id+1)))
+    model.eval()
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm_notebook(test_dataloader)):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+        valid_length= valid_length
+        label = label.long().to(device)
+        out = model(token_ids, valid_length, segment_ids)
+        test_acc += calc_accuracy(out, label)
+    print("epoch {} test acc {}".format(e+1, test_acc / (batch_id+1)))
+
